5ff0a686aada592af7804354fc15385990a8b887
Parvv04 <parzzabeth12@gmail.com>
2025-08-24 03:50:47 +0000

    frontend

---
+++ src/components/figma/ImageWithFallback.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+interface ImageWithFallbackProps {
+  src: string;
+  alt: string;
+  className?: string;
+  fallbackSrc?: string;
+}
+
+export const ImageWithFallback: React.FC<ImageWithFallbackProps> = ({
+  src,
+  alt,
+  className,
+  fallbackSrc = '/default-image.jpg'
+}) => {
+  const [imgSrc, setImgSrc] = React.useState(src);
+
+  const handleError = () => {
+    setImgSrc(fallbackSrc);
+  };
+
+  return (
+    <img
+      src={imgSrc}
+      alt={alt}
+      className={className}
+      onError={handleError}
+    />
+  );
+};
